9d6450e6de0a494049c2239549e7ecd47527f509
GodsaintBHH26 <kabirajarijit40@gmail.com>
2025-02-02 07:10:43 +0530

    created a custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Footer from "./components/footer";
-import { Profile } from "./components/profile";
+import useLocalStorage from "./useLocalStorage";
 
 function App() {
 
-
-
+  const [name, setName] = useLocalStorage('username', '');
+  const [id, setId] = useLocalStorage('userid', '');
 
   return (
     <>
-    <Profile/>
-    <Footer/>
+    <input type="text" placeholder="Enter Your Name" value={name} onChange={e=>setName(e.target.value)} />
+    <h2>Hello, {name} </h2>
+
+    <input type="text" placeholder="Enter Your Name" value={id} onChange={e=>setId(e.target.value)} />
+    <h2>Your ID: {id} </h2>
     </>
   );
 }
